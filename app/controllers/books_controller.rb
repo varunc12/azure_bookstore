7f682cb30fd273f90227a933d5cf22af5c5f90f1
Daniel <danielwilder@mauras-mbp.attlocal.net>
2017-06-28 17:05:16 -0700

    now can add books to db and view books [index page] via localhost:3000/books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,28 @@
 class BooksController < ApplicationController
+    def index
+      @books = Book.all
+    end
+
+    def show
+      @book = Book.find(params[:id])
+    end
+
     def new
     end
 
     def create
-      # render plain: params[:article].inspect
+      @book = Book.new(book_params)
+
+      @book.save
+      redirect_to @book
+    end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :author_name, :subtitle, :published, :publisher, :rating, :pages, :description, :image, :review, :amazon_link, :date_added)
     end
 end
 
+# , :subtitle, :published, :publisher, :rating, :pages, :description, :image, :review, :amazon_link, :date_added
 # bin/rails generate model Book title:string subtitle:string published:date rating:float pages:int description:text publisher:string image:string review:text amazon_link:string date_added:timestamp
+# render plain: params[:article].inspect
