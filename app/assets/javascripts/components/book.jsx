c49548ea06019b2864e00de4f2f7a1913f643bc6
Maura Wilder <maura@devpartners.com>
2017-08-03 15:30:01 -0400

    bootstrap added

--- app/assets/javascripts/components/book.jsx
+++ app/assets/javascripts/components/book.jsx
@@ -5,6 +5,7 @@ var Book = React.createClass({
         <div>
             <h3>{this.props.book.title}</h3>
             <img src={this.props.book.cover}/>
+            <p>{this.props.book.description}</p>
         </div>
     )
   }
--- app/assets/javascripts/components/booklist.jsx
+++ app/assets/javascripts/components/booklist.jsx
@@ -2,6 +2,7 @@ var BookList = React.createClass({
   render: function() {
     return (
         <div>
+            <Hello name="Daniel" />
             {this.props.books.map(function(book){
                 return (
                     <Book book={book} />
